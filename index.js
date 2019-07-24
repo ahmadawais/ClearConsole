d44f43f0e242a7c5e97f9a8e58089b53972e1ba7
Ahmad Awais ⚡️ <mrahmadawais@gmail.com>
2019-07-24 19:47:10 +0500

    📖 DOC: Improve text

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
  * Cross platform clear console.
  *
- * Platform: Windows, Linux, macOS.
+ * Platform: macOS, Windows, and Linux.
  *
- * @author <Ahmad Awais> (https://AhmadAwais.com/)
+ * @author ahmadawais <github.com/ahmadawais>
  */
 module.exports = () => process.stdout.write(process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H');
